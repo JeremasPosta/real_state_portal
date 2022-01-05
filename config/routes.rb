d79eeb49d3a95ec6c74efc84464c4247adfc28f9
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2022-01-05 14:34:26 -0300

    feat(routes): properties#contact added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'properties#index'
   get 'status/index'
   get 'properties', to: 'properties#index'
   get 'properties/:id', to: 'properties#show', as: 'property'
+  post 'properties/contact', to: 'properties#contact'
 end
