0e6de75be5847db1f3a500af81be08f9ad713155
Jeremias Ramirez <60799999+JeremasPosta@users.noreply.github.com>
2022-01-10 15:39:54 -0300

    test(properties): refactored until improve tests.

--- spec/views/properties/index.html.erb_spec.rb
+++ spec/views/properties/index.html.erb_spec.rb
@@ -4,12 +4,6 @@ RSpec.describe "properties/index.html.erb", type: :feature do
   it 'shows the right content', :vcr do
     visit properties_path
     expect(page).to have_content('Properties list:')
-    expect(page).to have_css('table')
-    expect(page).to have_content('Public ID')
-    expect(page).to have_content('Title')
-    expect(page).to have_content('Type')
-    expect(page).to have_content('Location')
-    expect(page).to have_content('Image')
   end
 
   it 'render 15 properties', :vcr do
